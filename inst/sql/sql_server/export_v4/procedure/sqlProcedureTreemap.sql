2c8738e79f8c60de2cd64004cfff3015044c4d4d
alondhe <alondhe@amgen.com>
2020-03-21 22:19:19 -0500

    Added ICD10PCS to procedure treemap export

--- inst/sql/sql_server/export_v4/procedure/sqlProcedureTreemap.sql
+++ inst/sql/sql_server/export_v4/procedure/sqlProcedureTreemap.sql
@@ -25,7 +25,7 @@ from (select * from @results_database_schema.ACHILLES_results where analysis_id 
 			inner join @vocab_database_schema.vocabulary v1
 			on c1.vocabulary_id = v1.vocabulary_id
 		where (
-			c1.vocabulary_id in (3,4,5)
+			c1.vocabulary_id in (3,4,5,35)
 			or (c1.vocabulary_id = 1 and c1.concept_class = 'Procedure')
 			)
 		) procs
