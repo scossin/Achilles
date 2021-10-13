1d654ed61792de5be41b34ac40f397b66483372f
cossins <sebastien.cossin@u-bordeaux.fr>
2021-10-13 08:06:50 +0200

    improve performance of query 110 by creating a temporary table

--- inst/sql/sql_server/analyses/110.sql
+++ inst/sql/sql_server/analyses/110.sql
@@ -2,6 +2,16 @@
 -- Note: using temp table instead of nested query because this gives vastly improved performance in Oracle
 
 --HINT DISTRIBUTE_ON_KEY(stratum_1)
+
+CREATE TABLE TEMP_110 AS
+SELECT DISTINCT 
+    EXTRACT(YEAR FROM observation_period_start_date)*100 + EXTRACT(MONTH FROM observation_period_start_date) AS obs_month,
+    TO_DATE(TO_CHAR(EXTRACT(YEAR FROM observation_period_start_date),'0000')||'-'||TO_CHAR(EXTRACT(MONTH FROM observation_period_start_date),'00')||'-'||TO_CHAR(1,'00'), 'YYYY-MM-DD')
+    AS obs_month_start,
+    TO_DATE(to_char(last_day(observation_period_start_date),'YYYY-MM-DD')||' 23:59:59','YYYY-MM-DD HH24:MI:SS') AS obs_month_end
+  FROM OMOP.observation_period
+  ORDER BY OBS_MONTH;
+
 SELECT
   110 as analysis_id,  
 	CAST(t1.obs_month AS VARCHAR(255)) as stratum_1,
@@ -11,15 +21,13 @@ into @scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_110
 FROM
 @cdmDatabaseSchema.observation_period op1
 join 
-(
-  SELECT DISTINCT 
-    YEAR(observation_period_start_date)*100 + MONTH(observation_period_start_date) AS obs_month,
-    DATEFROMPARTS(YEAR(observation_period_start_date), MONTH(observation_period_start_date), 1)
-    AS obs_month_start,
-    EOMONTH(observation_period_start_date) AS obs_month_end
-  FROM @cdmDatabaseSchema.observation_period
-) t1 on	op1.observation_period_start_date <= t1.obs_month_start
+TEMP_110 t1 on	op1.observation_period_start_date <= t1.obs_month_start
 	and	op1.observation_period_end_date >= t1.obs_month_end
 group by t1.obs_month;
+
+DROP TABLE TEMP_110;
+
+
+
 
 
